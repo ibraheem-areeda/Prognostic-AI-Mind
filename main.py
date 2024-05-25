476fa2e5acf9f009d3f05b00a5488e827d5dd53c
ibraheem-areeda <ibraheem.areeda@gmail.com>
2024-05-25 18:25:28 +0300

    CORSMiddleware

--- main.py
+++ main.py
@@ -6,12 +6,23 @@ import os
 
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.ANN import ANNPredictor
 from core.LR import LRPredictor
 from core.SVM import SVMPredictor
 
 app = FastAPI() 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/") 
 async def main_route(): 
