5463a026ddb0f87972fe8d96b0ca48386f19bc06
ibraheem-areeda <ibraheem.areeda@gmail.com>
2024-05-25 04:24:34 +0300

    final7

--- core/ANN.py
+++ core/ANN.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
